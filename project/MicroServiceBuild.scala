01126794ccf2ba7762b46b423bb69181ccd186fc
Jenkins HMRC <jenkins-hmrc@digital.cabinet-office.gov.uk>
2018-02-01 14:23:56 +0000

    Creating new service vat-subscription

---
+++ project/MicroServiceBuild.scala
@@ -0,0 +1,27 @@
+import sbt._
+import play.sbt.PlayImport._
+import play.core.PlayVersion
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object MicroServiceBuild extends Build with MicroService {
+
+  val appName = "vat-subscription"
+
+  override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
+
+  val compile = Seq(
+    "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0",
+    ws,
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.3.0"
+  )
+
+  def test(scope: String = "test,it") = Seq(
+    "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
+    "org.scalatest" %% "scalatest" % "2.2.6" % scope,
+    "org.pegdown" % "pegdown" % "1.6.0" % scope,
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+  )
+
+}
