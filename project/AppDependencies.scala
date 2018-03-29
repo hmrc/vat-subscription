0292d2f58935aa8f428077aca39d685dc892c9f3
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2018-03-29 09:04:08 +0100

    dependancy updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,7 +14,7 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0",
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.4.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.5.0",
     "org.typelevel" %% "cats-core" % catsVersion
   )
 
