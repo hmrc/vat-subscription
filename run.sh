3a9d0232ca23f5ad8237ace656931f7a7e89093d
MJCallahanPage <matthew.page@digital.hmrc.gov.uk>
2018-06-27 10:22:14 +0100

    [logback-tweak] cleaner logging in tests and running from local script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sbt 'run 9567 -Dapplication.router=testOnlyDoNotUseInAppConf.Routes'
+sbt 'run 9567 -Dlogger.resource=logback-test.xml -Dapplication.router=testOnlyDoNotUseInAppConf.Routes'
