21fb9e7baee75fb6dad0727b97e5df64704fb210
“JayKL” <43889094+JayKL@users.noreply.github.com>
2019-11-28 14:34:18 +0000

    added read Json read tests for ZeroRatedExmplApplication

---
+++ test/uk/gov/hmrc/vatsubscription/models/updateVatSubscription/request/deregistration/ZeroRatedExmpApplicationSpec.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsubscription.models.updateVatSubscription.request.deregistration
+
+import play.api.libs.json.{JsString, Json}
+import uk.gov.hmrc.play.test.UnitSpec
+import uk.gov.hmrc.vatsubscription.helpers.ZeroRatedExmpApplicationConstants._
+
+class ZeroRatedExmpApplicationSpec extends UnitSpec {
+
+  "ZeroRatedExmpApplication" when {
+
+    "deserializing JSON" should {
+
+      "return correct ZeroRatedExmpApplication when valid JSON max is recieved" in {
+        zeroRatedExmpApplicationFrontendJsonMax.as[ZeroRatedExmpApplication] shouldBe zeroRatedExmpApplicationFrontendJsonMax
+      }
+
+      "return correct ZeroRatedExmpApplication when valid JSON min is recieved" in {
+        zeroRatedExmpApplicationFrontendJsonMin.as[ZeroRatedExmpApplication] shouldBe zeroRatedExmpApplicationFrontendJsonMin
+      }
+
+    }
+  }
+
+}
